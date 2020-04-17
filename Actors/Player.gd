9384f64e1cdd90f22ee72409f0893efdcfb7f5a0
Tony Head <tony.head@dws.la>
2020-04-16 20:23:50 -0700

    Create player scene with basic move and slide and collision detection

--- Actors/Player.gd
+++ Actors/Player.gd
@@ -1,4 +1,20 @@
 extends KinematicBody2D
 
-func _ready():
-  print("Hello world.")
+const ACCELERATION = 500
+const MAX_SPEED = 150
+const FRICTION = 500
+
+var velocity = Vector2.ZERO
+
+func _physics_process(dt):
+    var input_vector = Vector2.ZERO
+    input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+    input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+    input_vector = input_vector.normalized()
+
+    if input_vector != Vector2.ZERO:
+        velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * dt)
+    else:
+        velocity = velocity.move_toward(Vector2.ZERO, FRICTION * dt)
+
+    velocity = move_and_slide(velocity)
